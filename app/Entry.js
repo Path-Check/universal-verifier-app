b8ebeb7d5783c3e68f7c432eaab9365b857a5748
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-16 17:27:08 -0500

    removing unused function

--- app/Entry.js
+++ app/Entry.js
@@ -18,15 +18,6 @@ const PUB_KEY = "-----BEGIN PUBLIC KEY-----\n"+
 "gwQco1KRMDSmXSMkDwIDAQAB\n"+
 "-----END PUBLIC KEY-----\n";
 
-function str2ab(str) {
-  var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
-  var bufView = new Uint16Array(buf);
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
 class Entry extends Component {
   constructor(props) {
         super(props);
