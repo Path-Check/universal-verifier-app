b172b39640eeeaa98c87679738ffb4190ea82d6b
Vitor Pamplona <vitor@vitorpamplona.com>
2020-11-18 09:26:34 -0500

    Changing App Name

--- android/app/src/main/java/org/pathcheck/healthpassport/reader/MainActivity.java
+++ android/app/src/main/java/org/pathcheck/healthpassport/reader/MainActivity.java
@@ -1,4 +1,4 @@
-package com.vaccinecertificatetrackingapp;
+package org.pathcheck.healthpassport.reader;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "VaccineCertificateTrackingApp";
+    return "HealthPassportReader";
   }
 }
--- android/app/src/main/java/org/pathcheck/healthpassport/reader/MainApplication.java
+++ android/app/src/main/java/org/pathcheck/healthpassport/reader/MainApplication.java
@@ -1,4 +1,4 @@
-package com.vaccinecertificatetrackingapp;
+package org.pathcheck.healthpassport.reader;
 
 import android.app.Application;
 import android.content.Context;
