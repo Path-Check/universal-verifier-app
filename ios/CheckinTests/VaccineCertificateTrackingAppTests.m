8ee09b172a78d336e53aacef6f655816bbc086a2
Vitor Pamplona <vitor@vitorpamplona.com>
2021-03-11 13:21:35 -0500

    Finishing setup for Xcode.

--- ios/CheckinTests/VaccineCertificateTrackingAppTests.m
+++ ios/CheckinTests/VaccineCertificateTrackingAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VaccineCertificateTrackingAppTests : XCTestCase
+@interface CheckinTests : XCTestCase
 
 @end
 
-@implementation VaccineCertificateTrackingAppTests
+@implementation CheckinTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
