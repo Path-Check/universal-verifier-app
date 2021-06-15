cd66521331a751a00487060593d2e9f4f7462634
Vitor Pamplona <vitor@vitorpamplona.com>
2021-06-15 14:42:12 -0400

    Slightly changing the color of the card to match figma

--- app/themes/Colors.js
+++ app/themes/Colors.js
@@ -2,8 +2,9 @@
 export const lightColors = {
   background: '#F8F8FF',
   divisor: 'rgba(155, 160, 170, 0.4)', 
-  primary: 'rgba(56, 172, 236, 1)',
+  primary: 'rgba(64, 81, 219, 1)',
   text: '#121212',
+  cardText: '#FFF',
   error: '#D32F2F',
 };
 
@@ -11,7 +12,7 @@ export const lightColors = {
 export const darkColors = {
   background: '#121212',
   divisor: 'rgba(155, 160, 170, 0.4)', 
-  primary: 'rgba(56, 172, 236, 1)',
+  primary: 'rgba(64, 81, 219, 1)',
   text: '#FFFFFF',
   error: '#EF9A9A',
 };
