2a6e05fefb1fbe917be9efb4fea57209bdfcf79c
Vitor Pamplona <vitor@vitorpamplona.com>
2021-10-07 15:49:27 -0400

    Refining DIVOC's card for India, SriLanka and Philippines

--- app/components/CowinCard.js
+++ app/components/CowinCard.js
@@ -8,93 +8,205 @@ import Moment from 'moment';
 import { CardStyles as styles } from '../themes/CardStyles' 
 
 const TRUST_REGISTRY = {
-	"did:india": "Republic of India",
-	"did:srilanka:moh":  "Republic of Sri Lanka",
-	"did:philippines": "Republic of the Philippines"
+	"did:india": "Gov of India",
+	"did:srilanka:moh":  "Gov of Sri Lanka",
+	"did:philippines": "Gov of Philippines"
 }
 
 export default class CowinCard extends Component {
-	issuerName = (card) => {
-		if (TRUST_REGISTRY[card.pub_key.toLowerCase()]) {
-			return TRUST_REGISTRY[card.pub_key.toLowerCase()]
-		}
-		return card.pub_key.toLowerCase();
-	}
+  issuerName = card => {
+    if (TRUST_REGISTRY[card.pub_key.toLowerCase()]) {
+      return TRUST_REGISTRY[card.pub_key.toLowerCase()];
+    }
+    return card.pub_key.toLowerCase();
+  };
 
-	issuedAt = (card) => {
-		return Moment(card.cert.issuanceDate).format('MMM DD, YYYY');
-	}
+  issuedAt = card => {
+    return Moment(card.cert.issuanceDate).format('MMM DD, YYYY');
+  };
 
-	showQR = (card) => {
-    this.props.navigation.navigate({name: 'QRShow', params: {
-        qr: card.rawQR, 
-        title: card.cert.credentialSubject.name, 
+  showQR = card => {
+    this.props.navigation.navigate({
+      name: 'QRShow',
+      params: {
+        qr: card.rawQR,
+        title: card.cert.credentialSubject.name,
         detail: card.cert.credentialSubject.id.substring(4),
-        signedBy: this.issuerName(card) + " on " + this.issuedAt(card)
-      }
+        signedBy: this.issuerName(card) + ' on ' + this.issuedAt(card),
+      },
     });
+  };
+
+  formatPerson = () => {
+    if (this.cert().credentialSubject.name) {
+      let names = this.cert().credentialSubject.name.trim().split(' ');
+      for (i = 1; i < names.length - 1; i++) {
+        names[i] = names[i][0];
+      }
+      return names.join(' ');
+    } else return undefined;
+  };
+
+  cert = () => {
+    return this.props.detail.cert ? this.props.detail.cert : this.props.detail;
+  };
+
+  formatDoB = () => {
+    let str = [];
+
+		if (this.cert().credentialSubject.id && this.cert().credentialSubject.id !== 'NA')
+      str.push(this.cert().credentialSubject.id.replace('did:', '').replace(' Card',""));
+		else 
+	  if (this.cert().credentialSubject.refId)
+      str.push("ID: " + this.cert().credentialSubject.refId);
+
+    if (this.cert().credentialSubject.age)
+      str.push(this.cert().credentialSubject.age + 'yrs');
+
+    if (this.cert().credentialSubject.gender)
+      str.push(this.cert().credentialSubject.gender);
+
+    if (this.cert().credentialSubject.sex)
+      str.push(this.cert().credentialSubject.sex);
+
+    return str.join(', ');
+  };
+
+	formatManufacturer = (item) => {
+		if (item.vaccine === item.manufacturer)
+			return undefined;
+
+		if (this.cert().proof.verificationMethod !== "did:india") 
+			return item.vaccine;
+		else 
+			return item.manufacturer;
+	}
+
+	formatBrand = (item) => {
+		if (this.cert().proof.verificationMethod === "did:india") 
+			return item.vaccine;
+		else 
+			return item.manufacturer.replace(/\([^()]*\)/g, "")
+	}
+
+	formatBatch = (item) => {
+		if (item.batch)
+			return item.batch.replace(item.vaccine, "").replace(" -","").replace("#","").replace(" ", "");
+		return undefined;
+	}
+
+  renderCard = () => {
+    console.log(this.cert().evidence[0]);
+    return (
+      <View style={[styles.card, {backgroundColor: this.props.colors.primary}]}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text style={styles.notes}>
+            {Moment(this.props.detail.scanDate).format('MMM DD, hh:mma')} -
+            COVID Vaccine
+          </Text>
+          <FontAwesome5
+            style={styles.button}
+            name={'trash'}
+            onPress={() => this.props.removeItem(this.props.detail.signature)}
+            solid
+          />
+        </View>
+
+        {this.formatPerson() && (
+          <View style={styles.row}>
+            <Text style={styles.title}>{this.formatPerson()}</Text>
+          </View>
+        )}
+
+        {this.formatDoB() && (
+          <View style={styles.row}>
+            <Text style={styles.notes}>{this.formatDoB()}</Text>
+          </View>
+        )}
+
+        <Divider
+	          style={[
+            styles.divisor,
+            {borderBottomColor: this.props.colors.cardText},
+          ]}
+        />
+
+        <FlatList
+          data={this.cert().evidence}
+          keyExtractor={item => item.certificateId}
+          renderItem={({item}) => {
+            return (
+              <View>
+								<View  style={{alignItems: 'center'}}>
+									<Text style={styles.subtitle}>COVID Vaccine {item.dose} of {item.totalDoses}</Text>
+								</View>
+
+								<View style={{alignItems: 'center'}}>
+                  <Text style={styles.notes}>{this.formatBrand(item)} #{this.formatBatch(item)}</Text>
+                </View>
+
+								<View style={{alignItems: 'center'}}>
+									<Text style={styles.notes}>{Moment(item.date).format('MMM DD, YYYY')}</Text>
+								</View>
+
+								{ item.certificateId && 
+								<View style={{alignItems: 'center'}}>
+									<Text style={styles.notes}>Cert ID: {item.certificateId}</Text>
+                </View>
+								}
+
+								{ item.effectiveStart && 
+								<View style={{alignItems: 'center'}}>
+									<Text style={styles.notes}>Effective from {Moment(item.effectiveStart).format('MMM DD')} to {Moment(item.effectiveUntil).format('MMM DD, YYYY')}</Text>
+								</View>
+								}
+
+								{ this.formatManufacturer(item) && 
+                <View style={{alignItems: 'center'}}>
+                  <Text style={styles.notesSmall}>{this.formatManufacturer(item)}</Text>
+                </View>
+								}
+
+								{ item.prophylaxis && 
+								<View style={{alignItems: 'center'}}>
+                  <Text style={styles.notesSmall}>{item.prophylaxis}</Text>
+                </View>
+								}
+              </View>
+            );
+          }}
+        />
+
+        <Divider
+          style={[
+            styles.divisor,
+            {borderBottomColor: this.props.colors.cardText},
+          ]}
+        />
+
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <FontAwesome5 style={styles.icon} name={'check-circle'} solid />
+          <Text style={styles.notes}>
+            {this.issuerName(this.props.detail)} on{' '}
+            {this.issuedAt(this.props.detail)}
+          </Text>
+        </View>
+      </View>
+    );
+  };
+
+  render() {
+    return this.props.pressable ? (
+      <TouchableOpacity onPress={() => this.showQR(this.props.detail)}>
+        {this.renderCard()}
+      </TouchableOpacity>
+    ) : (
+      this.renderCard()
+    );
   }
-
-	renderCard = () => {
-		return (
-			<View style={[styles.card, {backgroundColor:this.props.colors.primary}]}>
-				<View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-					<Text style={styles.notes}>{Moment(this.props.detail.scanDate).format('MMM DD, hh:mma')} - COVID Vaccine</Text>
-					<FontAwesome5 style={styles.button} name={'trash'} onPress={() => this.props.removeItem(this.props.detail.signature)} solid/>
-				</View>
-
-				<View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-					<Text style={styles.title}>{this.props.detail.cert.credentialSubject.name}</Text>
-				</View>
-
-				<View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-					<Text style={styles.notes}>{this.props.detail.cert.credentialSubject.age} yrs-old {this.props.detail.cert.credentialSubject.gender}</Text>
-				</View>
-
-				<View style={{flexDirection:'row', justifyContent:'space-between', alignItems:'center'}}>
-						<Text style={styles.notes}>{this.props.detail.cert.credentialSubject.id.substring(4)}</Text>
-				</View>
-				
-				<Divider style={[styles.divisor, {borderBottomColor:this.props.colors.cardText}]} />
-
-				<FlatList 
-					data={this.props.detail.cert.evidence} 
-					keyExtractor={item => item.certificateId} 
-					renderItem={({item}) => {
-							return (	<View>
-							<View style={styles.row}>
-								<Text style={styles.notes}>Shots taken: {item.dose} of {item.totalDoses}</Text>
-							</View>
-							
-							<View style={styles.row}>
-								<Text style={styles.notes}>
-										Vaccine: {item.vaccine} #{item.batch}
-								</Text>
-							</View>
-								
-							<View style={styles.row}>
-								<Text style={styles.notes}>
-										{item.manufacturer}
-								</Text>
-							</View>
-							</View>)
-					}} />
-					
-				<Divider style={[styles.divisor, {borderBottomColor:this.props.colors.cardText}]} />
-				
-				<View style={{flexDirection:'row', alignItems: 'center'}}>
-					<FontAwesome5 style={styles.icon} name={'check-circle'} solid/>
-					<Text style={styles.notes}>{this.issuerName(this.props.detail)} on {this.issuedAt(this.props.detail)}</Text>
-				</View>
-			</View>
-		)
-	}
-
-	render() {
-		return this.props.pressable ? 
-		( <TouchableOpacity onPress={() => this.showQR(this.props.detail)}>
-				{this.renderCard()}
-			</TouchableOpacity>
-		) : this.renderCard();
-	}
 }
