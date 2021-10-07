7803512ba6a6ec9d6c6cdfda9ef4dc81574511b5
Vitor Pamplona <vitor@vitorpamplona.com>
2021-10-07 15:48:30 -0400

    Adding a QR Decoder due to lack of decoding of byte mode qrs from ReactNative

---
+++ app/screens/QRDecoder/BitStream.js
@@ -0,0 +1,51 @@
+export class BitStream {
+  constructor(bytes) {
+    this.bytes = bytes;
+    this.byteOffset = 0;
+    this.bitOffset = 0;
+  }
+
+  readBits(numBits) {
+    if (numBits < 1 || numBits > 32 || numBits > this.available()) {
+      throw new Error("Cannot read " + numBits.toString() + " bits");
+    }
+
+    let result = 0;
+    // First, read remainder from current byte
+    if (this.bitOffset > 0) {
+      const bitsLeft = 8 - this.bitOffset;
+      const toRead = numBits < bitsLeft ? numBits : bitsLeft;
+      const bitsToNotRead = bitsLeft - toRead;
+      const mask = (0xFF >> (8 - toRead)) << bitsToNotRead;
+      result = (this.bytes[this.byteOffset] & mask) >> bitsToNotRead;
+      numBits -= toRead;
+      this.bitOffset += toRead;
+      if (this.bitOffset === 8) {
+        this.bitOffset = 0;
+        this.byteOffset++;
+      }
+    }
+
+    // Next read whole bytes
+    if (numBits > 0) {
+      while (numBits >= 8) {
+        result = (result << 8) | (this.bytes[this.byteOffset] & 0xFF);
+        this.byteOffset++;
+        numBits -= 8;
+      }
+
+      // Finally read a partial byte
+      if (numBits > 0) {
+        const bitsToNotRead = 8 - numBits;
+        const mask = (0xFF >> bitsToNotRead) << bitsToNotRead;
+        result = (result << numBits) | ((this.bytes[this.byteOffset] & mask) >> bitsToNotRead);
+        this.bitOffset += numBits;
+      }
+    }
+    return result;
+  }
+
+  available() {
+    return 8 * (this.bytes.length - this.byteOffset) - this.bitOffset;
+  }
+}
